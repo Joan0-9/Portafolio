533556683e46194827ae4b291a81c3d410bfcca3
JoanBotinaUNV <joan.botina@correounivalle.edu.co>
2025-02-10 12:56:48 -0500

    portafolio 2025

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'Portafolio';
+
+  constructor(private router: Router){
+  }
+  
+  renderAboutMe(){
+    this.router.navigate(['app-about']);
+  }
+
+  renderProject(){
+    this.router.navigate(['app-project']);
+  }
+
+  renderContact(){
+    this.router.navigate(['app-contact']);
+  }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './Pages/about/about.component';
+import { ProjectComponent } from './Pages/project/project.component';
+import { ContactComponent } from './Pages/contact/contact.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path :'app-about', component:AboutComponent},
+    {path :'app-project', component:ProjectComponent},
+    {path :'app-contact', component:ContactComponent},
+
+    {path :'**',redirectTo:'app-about', pathMatch:'full'}
+];
